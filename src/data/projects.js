9677591c8d7fce48c83102465b583cbef7629a60
Ayesha <ayeshashaw520@gmail.com>
2025-04-29 19:01:29 +0530

    new

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,6 +4,7 @@ import image2 from '../assets/image2.png';
 import image3 from '../assets/image3.png';
 import image4 from '../assets/image4.png';
 import image5 from '../assets/image5.png';
+import image6 from '../assets/image6.png';
 
 
 
@@ -21,6 +22,16 @@ const projects = [
   },
   {
     id: 2,
+    title: "InvestFolio – Investment portfolio Tracker",
+    description: "A full-stack investment portfolio tracking application built with the MERN stack (MongoDB, Express.js, React, Node.js). This application helps investors manage and track their investment portfolios across multiple asset classes including stocks, commodities, forex, bonds, ETFs, and cryptocurrencies.",
+    technologies: ["MongoDB", "Express", "React", "Node.js"],
+    githubUrl: "https://github.com/ayeshashaw/Investfolio-Investment-Portfolio-Tracker",
+    liveUrl: " https://investfolio.vercel.app",
+    featured: true,
+    imageUrl: image6
+  },
+  {
+    id: 3,
     title: "Sorting-Visualizer",
     description: "The Sorting Visualizer is an interactive tool designed to help users understand the workings of popular sorting algorithms. By providing a visual representation of the sorting process, this project simplifies the grasp of fundamental concepts in computer science and algorithm design. It serves as an ideal educational resource for students, coding enthusiasts, and anyone curious about how sorting algorithms function.",
     technologies: ["HTML", "CSS", "JavaScript"],
@@ -31,7 +42,7 @@ const projects = [
   },
 
   {
-    id: 3,
+    id: 4,
     title: "Calculator",
     description: "A basic yet functional Calculator application built with React.js, enabling users to perform arithmetic operations like addition, subtraction, multiplication, and division. It features a responsive UI, real-time updates using React state, and a clean, user-friendly design.",
     technologies: ["React", "CSS","javaScript"],
@@ -42,7 +53,7 @@ const projects = [
   },
 
   {
-    id: 4,
+    id: 5,
     title: "Todo-App",
     description: "A simple and interactive To-Do List application built with React.js that allows users to add, edit, delete, and mark tasks as completed. It uses state management for dynamic updates and offers a clean, responsive UI for effective task tracking.",
     technologies: ["React", "CSS","javaScript"],
@@ -52,7 +63,7 @@ const projects = [
     imageUrl: image2
   },
   {
-    id: 5,
+    id: 6,
     title: "Quiz App",
     description: "A dynamic and interactive quiz application built with React, utilizing the Open Trivia Database API. Users can create quizzes with customizable options, including category, difficulty, and the number of questions.",
     technologies: ["React", "CSS", "Axios"],
